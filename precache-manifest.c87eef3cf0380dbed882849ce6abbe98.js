13f2208269135a1cb1e0a9c736cc05e2c4507459
Samuel-SantosPHBR <Samuel-SantosPHBR@users.noreply.github.com>
2022-05-26 12:22:08 +0000

    deploy: 2c9d7b04d3d3e37b68e84d023725aa2356782a42

---
+++ precache-manifest.c87eef3cf0380dbed882849ce6abbe98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db24fb748502e6f639f2ce655d26d3a",
+    "url": "/walletTemplate/index.html"
+  },
+  {
+    "revision": "f2b91ba42ba3e27fc9e2",
+    "url": "/walletTemplate/static/css/main.88fa4506.chunk.css"
+  },
+  {
+    "revision": "fad19cd4f7fd8d3cbc3b",
+    "url": "/walletTemplate/static/js/2.c2db8b4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/walletTemplate/static/js/2.c2db8b4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b91ba42ba3e27fc9e2",
+    "url": "/walletTemplate/static/js/main.7064f7d2.chunk.js"
+  },
+  {
+    "revision": "e65b57e562aef46cf6ca",
+    "url": "/walletTemplate/static/js/runtime-main.f5922443.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7956137ee2224af61f3ff14db7d86ae2.js"
+  "/walletTemplate/precache-manifest.c87eef3cf0380dbed882849ce6abbe98.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/walletTemplate/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
